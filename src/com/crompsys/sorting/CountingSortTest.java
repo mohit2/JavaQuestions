cd1c132e9e0ae6b28cd40d0c86b221003124463f
mohit gupta <guptamohit.me@gmail.com>
2014-10-09 15:25:40 +0530

    First Commit

---
+++ src/com/crompsys/sorting/CountingSortTest.java
@@ -0,0 +1,22 @@
+package com.crompsys.sorting;
+
+class CountingSort{
+	
+public static void sort(int[] arr , int n ) {
+	
+	System.out.println("sortrd");
+	
+}	
+	
+}
+
+public class CountingSortTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]={1,2};
+		CountingSort.sort(arr, 2);
+
+	}
+
+}
